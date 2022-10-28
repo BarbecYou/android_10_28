92fd0339fd072e8f00a73db3167ecfffd15dfc46
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-28 09:34:53 +0200

    kesz

--- app/src/main/java/hu/petrik/android_10_28/MainActivity.java
+++ app/src/main/java/hu/petrik/android_10_28/MainActivity.java
@@ -3,12 +3,23 @@ package hu.petrik.android_10_28;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnKutya;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnKutya = findViewById(R.id.btnKutya);
+        btnKutya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, getString(R.string.kutya_toast), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
